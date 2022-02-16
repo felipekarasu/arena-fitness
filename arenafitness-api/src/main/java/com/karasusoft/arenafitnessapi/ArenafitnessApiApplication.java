447db16de7fa06837bb752950ad435e9ca392c03
Felipe Grego <felipe.abgr@gmail.com>
2022-02-15 23:03:29 -0300

    initial configutarions to run the application

---
+++ arenafitness-api/src/main/java/com/karasusoft/arenafitnessapi/ArenafitnessApiApplication.java
@@ -0,0 +1,13 @@
+package com.karasusoft.arenafitnessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenafitnessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenafitnessApiApplication.class, args);
+	}
+
+}
