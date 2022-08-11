9c01723ae57715d859401ab38e3578c040354625
Usarak <felipe.abgr@gmail.com>
2022-08-11 00:52:28 -0300

    formating pc, saving changed local files

--- arenafitness-api/src/main/java/com/karasusoft/arenafitnessapi/ArenafitnessApiApplication.java
+++ arenafitness-api/src/main/java/com/karasusoft/arenafitnessapi/ArenafitnessApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArenafitnessApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ArenafitnessApiApplication.class, args);
 	}
